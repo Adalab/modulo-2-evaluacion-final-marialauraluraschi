301b0146e623cde7fb9024927bf4236332c075d1
Lau Luraschi <loud.luraschi@gmail.com>
2023-09-29 13:38:29 +0200

    no https in url. ul is dinamic. will need id later

--- src/js/main.js
+++ src/js/main.js
@@ -2,19 +2,20 @@
 //*Constantes:
 const searchInput = document.querySelector('.js-search-input'); //input
 const searchBtn = document.querySelector('.js-search-btn'); //botón buscar
-const matches = document.querySelector('.js-matches'); //ul
 let inputValue = '';
 
 //*Manejadora
 function handleSearch(event) {
   event.preventDefault();
   inputValue = searchInput.value;
-  let url = `https://api.tvmaze.com/search/shows?q=${inputValue}`;
+  let url = `//api.tvmaze.com/search/shows?q=${inputValue}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
       for (let i = 0; i < data.length; i++) {
+        let showId= data[i].show.id;
+        console.log(showId);
         let showName= data[i].show.name;
         console.log(showName);
         let showPic= data[i].show.image.medium;
