13c17f1f8aa22662efe9a30e291f9d6ecaf474f0
Lau Luraschi <loud.luraschi@gmail.com>
2023-10-02 13:31:57 +0200

    fixign stuff

--- src/js/main.js
+++ src/js/main.js
@@ -32,11 +32,14 @@ function handleSearch(event) {
 function renderShow(item) {
   let showId = item.show.id;
   let showName = item.show.name;
+
   let showPic = item.show.image
     ? item.show.image.medium
     : `https://via.placeholder.com/210x295/ffffff/666666/?text=${showName}`;
+  const isFav = favs.findIndex((fav) => fav.show.id === item.show.id);
+  let showClass = isFav === -1 ? '' : 'selected';
   let showItem = '';
-  showItem += `<li class="js-item" id="${showId}"><article>
+  showItem += `<li class="js-item ${showClass}" id="${showId}"><article>
   <img
   src=${showPic}
   alt="Picture of ${showName}"
@@ -65,14 +68,15 @@ function handleShow(event) {
   const indexFav = favs.findIndex((item) => item.show.id === clickedId);
   if (indexFav === -1) {
     favs.push(clickedMatch);
-    clicked.classList.add('selected');
+    // clicked.classList.add('selected');
   } else {
     favs.splice(indexFav, 1);
-    clicked.classList.remove('selected');
+    // clicked.classList.remove('selected');
   }
   let favsJSON = JSON.stringify(favs);
   localStorage.setItem('favoritos', favsJSON);
   renderList(favs, aside, 'favs');
+  renderList(matches, main, 'matches');
 }
 
 //*Arega event listeners a cada show
