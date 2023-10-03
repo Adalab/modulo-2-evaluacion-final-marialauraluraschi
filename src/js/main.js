45cdc314dfab1fb939dc1f110a0b59851ce38f5b
Maria Laura Luraschi <loud.luraschi@gmail.com>
2023-10-03 12:22:28 +0200

    interview

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ const searchBtn = document.querySelector('.js-search-btn');
 const main = document.querySelector('.js-main');
 const aside = document.querySelector('.js-aside');
 const resetBtn = document.querySelector('.js-rst-btn');
+const logBtn=document.querySelector('.js-log');
 let favs = [];
 let matches = [];
 let inputValue = '';
@@ -33,22 +34,30 @@ function handleSearch(event) {
 //*Renderizadora de serie
 function renderShow(item) {
   let showId = item.show.id;
+  let showLang = item.show.language;
   let showName = item.show.name;
-
   let showPic = item.show.image
     ? item.show.image.medium
     : `https://via.placeholder.com/210x295/ffffff/666666/?text=${showName}`;
   const isFav = favs.findIndex((fav) => fav.show.id === item.show.id);
   let showClass = isFav === -1 ? '' : 'selected';
+  let showRec= '';
+  if(showLang==='Spanish'){
+    showRec=`<p>Rencomenda</p>`;
+  }
   let showItem = '';
+
   showItem += `<li class="js-item ${showClass}" id="${showId}"><article>
   <img
   src=${showPic}
   alt="Picture of ${showName}"
   />
   <h3>${showName.toUpperCase()} </h3>
+  <p>Idioma: ${showLang}</p>
+  ${showRec}
    </article>
  </li>`;
+ 
   return showItem;
 }
 
@@ -67,13 +76,11 @@ function handleShow(event) {
   const clicked = event.currentTarget;
   const clickedId = parseInt(clicked.id);
   const clickedMatch = matches.find((item) => item.show.id === clickedId);
-  const indexFav = favs.findIndex((item) => item.show.id === clickedId);
+  const indexFav = favs.findIndex((fav) => fav.show.id === clickedId);
   if (indexFav === -1) {
     favs.push(clickedMatch);
-    // clicked.classList.add('selected');
   } else {
     favs.splice(indexFav, 1);
-    // clicked.classList.remove('selected');
   }
   let favsJSON = JSON.stringify(favs);
   localStorage.setItem('favoritos', favsJSON);
@@ -94,10 +101,14 @@ function handleReset() {
   localStorage.removeItem('favoritos');
   renderList(favs, aside, 'favs');
 }
+function handleLog(){
+  console.log(favs.length);
+}
 
 //*Listener
 searchBtn.addEventListener('click', handleSearch);
 resetBtn.addEventListener('click', handleReset);
+logBtn.addEventListener('click', handleLog);
 
 
 
